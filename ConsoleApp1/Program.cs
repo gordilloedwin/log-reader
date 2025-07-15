43e6028ec76e04034bbc638313664fb743dfe8a2
Edwin Hernandez Gordillo <gordilloedwin@hotmail.com>
2025-07-15 02:19:58 -0600

    initial commit
    initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string? mystring = "    ";
+
+if (!string.IsNullOrWhiteSpace(mystring))
+{
+    Console.WriteLine(mystring);
+}
+else
+{
+    Console.WriteLine("valid string into null");
+}
